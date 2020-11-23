7d9c2c88a8c42d48d64c540f507bbed384322969
Connor Shugg <cwshugg@vt.edu>
2020-11-23 04:14:22 +0000

    comented out testing main()

--- src/task.c
+++ src/task.c
@@ -94,20 +94,20 @@ char* task_to_string(Task* task)
 
 
 // ================================ Testing ================================ //
-#include "box.h"
-int main()
-{
-    Task* t1 = task_new("Task 1's Title", "Task 1's Description");
-    char* t1_string = task_to_string(t1);
-    printf("task with title and description:\n%s\n", t1_string);
-    free(t1_string);
+// #include "box.h"
+// int main()
+// {
+//     Task* t1 = task_new("Task 1's Title", "Task 1's Description");
+//     char* t1_string = task_to_string(t1);
+//     printf("task with title and description:\n%s\n", t1_string);
+//     free(t1_string);
 
-    Box* t1_box = box_new(0, 0, t1->title, t1->description);
-    box_adjust_to_text(t1_box, 0);
-    box_print(t1_box);
+//     Box* t1_box = box_new(0, 0, t1->title, t1->description);
+//     box_adjust_to_text(t1_box, 0);
+//     box_print(t1_box);
 
-    box_free(t1_box);
-    task_free(t1);    
+//     box_free(t1_box);
+//     task_free(t1);    
 
-    return 0;
-}
+//     return 0;
+// }
