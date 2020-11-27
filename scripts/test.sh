27cbd60cc70256c6ec34b30d592fbdbeeec26f5e
Connor Shugg <cwshugg@vt.edu>
2020-11-27 21:42:47 +0000

    added way to run test modules with valgrind

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,10 +12,13 @@ c_none="\033[0m"
 c_red="\033[0;31m"
 c_green="\033[0;32m"
 c_yellow="\033[1;33m"
+# run variables
+run_with_valgrind=0
 
 # if no argument was given, print a message
 if [ $# -lt 1 ]; then
     echo -e "${c_red}Error${c_none}: No test name given. Invocation: \"test.sh <test_module_name>\""
+    echo "Optionally you can invoke with: \"test.sh <test_module_name> valgrind\" to run with valgrind."
     echo ""
 
     # iterate and print all test names
@@ -25,6 +28,14 @@ if [ $# -lt 1 ]; then
     done
 
     exit 1
+fi
+
+# check for two arguments
+if [ $# -ge 2 ]; then
+    # if the second argument is 'valgrind', we'll run this with valgrind
+    if [[ $2 == "valgrind" ]]; then
+        run_with_valgrind=1
+    fi
 fi
 
 # run the makefile's 'make test' option
@@ -46,7 +57,11 @@ mv ./TEST $dir_tests/TEST
 # execute the test
 echo -e "${c_yellow}Executing test...${c_none}"
 cd $dir_tests
-./TEST
+if [ $run_with_valgrind -eq 1 ]; then   # run with valgrind:
+    valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./TEST
+else                                    # run without valgrind:
+    ./TEST
+fi
 
 # remove the executable once finished
 rm -f ./TEST
