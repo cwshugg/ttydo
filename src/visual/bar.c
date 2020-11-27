72259a03c3fe59319158fe9af349f4bf16409e52
Connor Shugg <cwshugg@vt.edu>
2020-11-27 04:20:04 +0000

    added the startings of a progress bar module

---
+++ src/visual/bar.c
@@ -0,0 +1,29 @@
+// A module that implements bar.h's definitions.
+//
+//      Connor Shugg
+
+// Module inclusions
+#include <stdlib.h>
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include "bar.h"
+
+// ========================== Progress Bar Struct ========================== //
+
+
+
+// ================================ Testing ================================ //
+#include <string.h>
+int main()
+{
+    printf("Test progress bar:\n\t");
+    for (int i = 0; i < 32; i++)
+    { printf("%s", PROGBAR_FILLED); }
+    for (int i = 0; i < 12; i++)
+    { printf("%s", PROGBAR_EMPTY); }
+    printf("\n");
+
+    char* x = PROGBAR_FILLED;
+    printf("Length of one extended char: %lu\n", strlen(x));
+}
---
+++ src/visual/bar.h
@@ -0,0 +1,23 @@
+// A header file that defines the data structure and functions used to
+// represent a progress bar.
+//
+//      Connor Shugg
+
+#ifndef BAR_H
+#define BAR_H
+
+// ========================= Constants and Macros ========================== //
+// Progress bar drawing definitions
+// https://www.utf8-chartable.de/unicode-utf8-table.pl?start=9472
+#define PROGBAR_FILLED "\u2588"     // for a filled progress bar block
+#define PROGBAR_EMPTY "\u2581"      // for an empty progress bar block
+#define PROGBAR_CHARACTER_SIZE 3    // number of bytes that make up each char
+
+
+// ========================== Progress Bar Struct ========================== //
+typedef struct _ProgressBar
+{
+
+} ProgressBar;
+
+#endif
