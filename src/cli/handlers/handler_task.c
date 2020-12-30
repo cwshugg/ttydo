94fb55373337ea114beb48d7fc605b0b5e5a63bc
Connor Shugg <cwshugg@vt.edu>
2020-12-30 15:34:57 +0000

    added the ability to toggle tasks as complete/incomplete

--- src/cli/handlers/handler_task.c
+++ src/cli/handlers/handler_task.c
@@ -14,7 +14,9 @@
 int handle_task_help(Command* comm, int argc, char** args);
 int handle_task_add(Command* comm, int argc, char** args);
 int handle_task_delete(Command* comm, int argc, char** args);
+int handle_task_mark(Command* comm, int argc, char** args);
 int truncate_string(char* original, char* copy, int max_length);
+Task* find_task(TaskList* list, char* input, char** title);
 
 
 // ============================== Initializer ============================== //
@@ -27,7 +29,7 @@ Command* init_command_task()
     if (!result) { return NULL; }
     
     // sub-commands
-    if (command_init_subcommands(result, 3)) { return NULL; }
+    if (command_init_subcommands(result, 4)) { return NULL; }
     result->subcommands[0] = command_new("Help", "h", "help",
         "Shows a list of supported sub-commands.",
         handle_task_help);
@@ -37,6 +39,9 @@ Command* init_command_task()
     result->subcommands[2] = command_new("Delete", "d", "delete",
         "Deletes a task from a task list.",
         handle_task_delete);
+    result->subcommands[3] = command_new("Mark", "m", "mark",
+        "Marks a given task as complete (or incomplete).",
+        handle_task_mark);
 
     // check each sub-command - if one wasn't initialized, return NULL
     for (int i = 0; i < result->subcommands_length; i++)
@@ -185,6 +190,7 @@ int handle_task_add(Command* comm, int argc, char** args)
     return 0;
 }
 
+// Handles the 'delete' sub command
 int handle_task_delete(Command* comm, int argc, char** args)
 {
     // if we don't have any lists, print and return
@@ -213,25 +219,16 @@ int handle_task_delete(Command* comm, int argc, char** args)
     }
     TaskList* list = tasklists[tl_index];
 
-    // attempt to convert the next argument to an integer (task index) OR
-    // use it as a title name for a task. Either way, we'll search the task
-    // list for the appropriate task.
-    char* end;
-    long index = strtol(args[1], &end, 10);
-    Task* task = NULL;
-    if (index > 0)
-    { task = task_list_get_by_index(list, index - 1); }
-    
-    // parse the title string and replace any strange characters with
-    // spaces
-    char title[TASK_TITLE_MAX_LENGTH + 1] = {'\0'};
-    int title_length = truncate_string(args[1], title,
-                                    TASK_TITLE_MAX_LENGTH);
-    replace_string_non_printables(title, title_length);
+    // if the list has no entries, return
+    if (list->size == 0)
+    {
+        printf("This list has no tasks.\n");
+        return 0;
+    }
 
-    // search by title if we dont have an index
-    if (index == 0)
-    { task = task_list_get_by_title(list, title); }
+    // next, attempt to find the task within the task list
+    char* title = NULL;
+    Task* task = find_task(list, args[1], &title);
 
     // if we didn't find a task, print and return
     if (!task)
@@ -240,8 +237,10 @@ int handle_task_delete(Command* comm, int argc, char** args)
                 list->name, title);
         fprintf(stderr, "Task numbers for this list must be between 1 and %d.\n",
                 list->size);
+        free(title);
         return 1;
     }
+    free(title);
 
     // attempt to remove the task from the list
     if (!task_list_remove(list, task))
@@ -249,6 +248,72 @@ int handle_task_delete(Command* comm, int argc, char** args)
     
     // free the removed task's memory and save the task list
     task_free(task);
+    if (save_task_list(list))
+    { fatality(1, "Failed to write to disk."); }
+
+    return 0;
+}
+
+// Handles the 'mark' sub-command
+int handle_task_mark(Command* comm, int argc, char** args)
+{
+    // if we don't have any lists, print and return
+    if (tasklist_array_length == 0)
+    {
+        printf("You don't have any task lists.\n");
+        return 0;
+    }
+
+    // if too few arguments were given, print a usage message
+    if (argc < 2)
+    {
+        print_usage("task mark <LIST> <TASK>");
+        printf("Where <LIST> is either a list's name or number.\n");
+        printf("Where <TASK> is either a task's name or number.\n");
+        return 0;
+    }
+
+    // take the first argument and attempt to locate the correct list
+    int tl_index = tasklist_array_find(args[0]);
+    if (tl_index < 0)
+    {
+        eprintf("Couldn't find a task list with name/number \"%s\".\n", args[0]);
+        fprintf(stderr, "Numbers must be between 1 and %d.\n", tasklist_array_length);
+        return 1;
+    }
+    TaskList* list = tasklists[tl_index];
+
+    // if the list has no entries, return
+    if (list->size == 0)
+    {
+        printf("This list has no tasks.\n");
+        return 0;
+    }
+    
+    // next, attempt to find the task within the task list
+    char* title = NULL;
+    Task* task = find_task(list, args[1], &title);
+
+    // if we didn't find a task, print and return
+    if (!task)
+    {
+        eprintf("Couldn't find a task within \"%s\" with name/number \"%s\".\n",
+                list->name, title);
+        fprintf(stderr, "Task numbers for this list must be between 1 and %d.\n",
+                list->size);
+        free(title);
+        return 1;
+    }
+    free(title);
+
+    // invert the 'is_complete' flag for the task
+    task->is_complete = !task->is_complete;
+    if (task->is_complete)
+    { printf("Task marked as complete.\n"); }
+    else
+    { printf("Task marked as incomplete.\n"); }
+    
+    // save the task list
     if (save_task_list(list))
     { fatality(1, "Failed to write to disk."); }
 
@@ -271,4 +336,35 @@ int truncate_string(char* original, char* copy, int max_length)
     // copy the string
     strncpy(copy, original, length);
     return length;
+}
+
+// Takes in a task list, and some sort of string input, and attempts to parse
+// it as either an index or a task title. If a task is found, its pointer is
+// returned. If one wasn't found, NULL is returned. The third parameter is used
+// to return the modified and parsed title string. It's dynamically allocated.
+Task* find_task(TaskList* list, char* input, char** cleaned_input)
+{
+    // attempt to convert the next argument to an integer (task index) OR
+    // use it as a title name for a task. Either way, we'll search the task
+    // list for the appropriate task.
+    char* end;
+    long index = strtol(input, &end, 10);
+    Task* task = NULL;
+    if (index > 0)
+    { task = task_list_get_by_index(list, index - 1); }
+    
+    // parse the title string and replace any strange characters with
+    // spaces
+    char* title = calloc(TASK_TITLE_MAX_LENGTH + 1, sizeof(char));
+    int title_length = truncate_string(input, title,
+                                    TASK_TITLE_MAX_LENGTH);
+    replace_string_non_printables(title, title_length);
+    // set the return parameter
+    *cleaned_input = title;
+
+    // search by title if we dont have an index
+    if (index == 0)
+    { task = task_list_get_by_title(list, title); }
+
+    return task;
 }
